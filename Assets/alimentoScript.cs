fa52df6acb7a190b28482bbbd99444a57a8a4675
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-06-06 12:09:54 -0300

    Terminado

---
+++ Assets/alimentoScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alimentoScript : MonoBehaviour
+{
+    public int valorAlimentario;
+    
+}
---
+++ Assets/personajeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+    
+    void tomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        alimentoScript alimento = other.GetComponent<alimentoScript>();
+        if (alimento)
+        {
+            tomarAlimento(alimento.valorAlimentario);
+            Destroy(other.gameObject);
+        }
+    }
+}
